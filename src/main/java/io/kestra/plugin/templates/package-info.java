28a37dca0376144a1da3fd096bad255bfa902040
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 12:22:21 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/templates/package-info.java
+++ src/main/java/io/kestra/plugin/templates/package-info.java
@@ -1,7 +1,7 @@
 @PluginSubGroup(
     title = "Example plugin",
     description = "A plugin to show how to build a plugin in Kestra.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.BUSINESS
 )
 package io.kestra.plugin.templates;
 

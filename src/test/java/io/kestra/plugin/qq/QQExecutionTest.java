be4f6535a252990feb2cfd856a03086eef646c45
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-11 00:52:11 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/qq/QQExecutionTest.java
+++ src/test/java/io/kestra/plugin/qq/QQExecutionTest.java
@@ -4,8 +4,9 @@ import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
 import io.kestra.core.runners.TestRunner;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -15,6 +16,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @KestraTest
 public class QQExecutionTest extends AbstractQQTest {
 
@@ -24,7 +26,7 @@ public class QQExecutionTest extends AbstractQQTest {
     @Inject
     protected LocalFlowRepositoryLoader repositoryLoader;
 
-    @BeforeEach
+    @BeforeAll
     protected void init() throws IOException, URISyntaxException {
         repositoryLoader.load(Objects.requireNonNull(QQExecutionTest.class.getClassLoader().getResource("flows")));
         this.runner.run();
@@ -37,7 +39,10 @@ public class QQExecutionTest extends AbstractQQTest {
             "tencent-qq"
         );
 
-        String receivedData = waitForWebhookData(() -> FakeWebhookController.data,5000);
+        String receivedData = waitForWebhookData(
+            () -> FakeWebhookController.data != null && FakeWebhookController.data.contains(failedExecution.getId()) ? FakeWebhookController.data : null,
+            5000
+        );
 
         assertThat(receivedData, containsString(failedExecution.getId()));
         assertThat(receivedData, containsString("https://mysuperhost.com/kestra/ui"));
